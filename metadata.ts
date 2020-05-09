bd9580287ebaa2c92951783d295c3108fc06d9d4
Junjie Chen <junjie.chen18@gmail.com>
2020-05-08 21:56:39 -0400

    feat(decorator): define logSpecification decorator

--- metadata.ts
+++ metadata.ts
@@ -1,7 +1,17 @@
+import 'reflect-metadata';
+
 class Plane {
   name: string = 'Boeing 777';
 
   pilot(): void {
     console.log('Plane is flying');
+  }
+}
+
+function logSpecification(target: typeof Plane): void {
+  for (let property in target.prototype) {
+    const model = Reflect.getMetadata('model', target.prototype, property);
+
+    console.log(`model: ${model}`);
   }
 }
